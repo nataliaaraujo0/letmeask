0cba8f0679a948236ffbd95c88c22960adbb84a1
Natalia <natalia.araujo@yazo.com.br>
2022-10-02 22:07:56 -0300

    fix: projeto inicial com thema

---
+++ styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    purple: {
+      "50": "#9875FB",
+      "90": "#1B0F25",
+    },
+    danger: {
+      "50": "#E73F5D",
+    },
+    pink: {
+      "20": "#D67EE2",
+      "50": "#E559F9",
+    },
+  },
+
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "purple.90",
+        color: "gray.300",
+      },
+    },
+  },
+});
