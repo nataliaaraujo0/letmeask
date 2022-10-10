29b8dab15c53302ba99509dafe0a631e1e5f4af6
Natalia <natalia.araujo@yazo.com.br>
2022-10-09 22:27:29 -0300

    criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NemRoom";
 
 function App() {
   return (
-    <>
-      <NewRoom />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
