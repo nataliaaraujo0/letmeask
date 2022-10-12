6bf3ea952de81ac9125e93d959f2d917bdbe2fc8
Natalia <natalia.araujo@yazo.com.br>
2022-10-12 16:37:19 -0300

    chore: configurando firebase

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../../letmeask/styles/theme";
+
+import "./services/firebase";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
